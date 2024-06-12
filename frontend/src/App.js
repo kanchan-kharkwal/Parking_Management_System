7b9aecfa327ccd0d4f49a5f895e062292b396898
Kanchan Kharkwal <159950316+kanchan-kharkwal@users.noreply.github.com>
2024-06-12 21:41:22 +0530

    Add files via upload
    Signed-off-by: Kanchan Kharkwal <159950316+kanchan-kharkwal@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route,  Routes } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Navbar from './components/common/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
-import Dashboard from './components/pages/JoinUs';
+import JoinUs from './components/pages/JoinUs';
 import Auth from './components/pages/Auth';
 import AdminLogin from './components/admin/AdminLogin';
 import AdminSignUp from './components/admin/AdminSignUp';
 import UserLogin from './components/user/UserLogin';
 import UserSignUp from './components/user/UserSignUp';
 import UserDashboard from './components/user/UserDashboard';
+import AdminDashboard from './components/admin/AdminDashboard';
+import ManageUsers from './components/admin/ManageUsers';
+
 import './App.css';
+import FAQ from './components/pages/FAQ';
+
 
 function App() {
   return (
@@ -26,7 +31,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/joinus" element={<JoinUs />} />
           <Route path="/admin-auth" element={<Auth />} />
           <Route path="/user-auth" element={<Auth />} />
           <Route path="/admin-login" element={<AdminLogin />} />
@@ -34,6 +40,8 @@ function App() {
           <Route path="/user-login" element={<UserLogin />} />
           <Route path="/user-signup" element={<UserSignUp />} />
           <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path='/admin-dashboard' element={<AdminDashboard />} />
+          <Route path="/manage-users" element={<ManageUsers />} />
         </Routes>
         </div>
         <Footer />
