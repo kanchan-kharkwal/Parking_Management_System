3fd3142aa177888e8a12de5350a4a7b062240f28
Kanchan Kharkwal <159950316+kanchan-kharkwal@users.noreply.github.com>
2024-06-20 14:41:17 +0530

    Update models.py
    Signed-off-by: Kanchan Kharkwal <159950316+kanchan-kharkwal@users.noreply.github.com>

--- backend/parking/models.py
+++ backend/parking/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 
 
 class Admin(models.Model):
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    password = models.CharField(max_length=100)
 
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -18,3 +22,10 @@ class ParkingSlot(models.Model):
     is_occupied = models.BooleanField(default=False)
     reserved_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     reserved_at = models.DateTimeField(blank=True, null=True)
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
