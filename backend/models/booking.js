8e2f9dcbfc94a7cebd4f2e886b821a9404ee55ea
kanchan-kharkwal <kharkwal.kanchan31@gmail.com>
2024-07-04 22:34:30 +0530

    update

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -4,7 +4,7 @@ const bookingSchema = new mongoose.Schema({
   slot: { type: mongoose.Schema.Types.ObjectId, ref: "Slot", required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   startTime: { type: Date, required: true },
-  vehicleNo: { type: String },
+  vehicleNo: { type: String, required: true},
   endTime: { type: Date, required: true },
   status: { type: String, enum: ["ongoing", "end"], default: "ongoing" },
 });
